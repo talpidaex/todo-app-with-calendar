ff831a42fdd93e0b75f703033f3e053790fa78a9
talpidaex <bayrakogzhn@gmail.com>
2023-12-27 23:49:50 +0300

    task manager structure created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ReduxProvider } from '@/redux/provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
